95da43b3a38147c48adf12d0c1197f08ca7219b2
Victor <melnikukr@mail.ru>
2015-09-01 12:13:29 +0300

    Add Exceptional example

--- app/src/main/java/com/annimon/java8streamexample/Utils.java
+++ app/src/main/java/com/annimon/java8streamexample/Utils.java
@@ -3,12 +3,12 @@ package com.annimon.java8streamexample;
 import android.content.Context;
 import android.util.Log;
 import com.annimon.stream.Collectors;
+import com.annimon.stream.Exceptional;
 import com.annimon.stream.Stream;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public final class Utils {
@@ -23,22 +23,23 @@ public final class Utils {
     }
 
     public static List<Word> readWords(Context context) {
-        final List<String> lines = new LinkedList<>();
-        try (final InputStream is = context.getAssets().open("words.txt");
-             final InputStreamReader isr = new InputStreamReader(is, "UTF-8");
-             final BufferedReader reader = new BufferedReader(isr)) {
-            String line;
-            while ( (line = reader.readLine()) != null ) {
-                lines.add(line);
+        return Exceptional.of(() -> {
+            final List<String> lines = new ArrayList<>();
+            try (final InputStream is = context.getAssets().open("words.txt");
+                 final InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+                 final BufferedReader reader = new BufferedReader(isr)) {
+                String line;
+                while ( (line = reader.readLine()) != null ) {
+                    lines.add(line);
+                }
             }
-        } catch (IOException e) {
-            Log.e("Java 8 Example", "Utils.readWords", e);
-        }
 
-        return Stream.of(lines)
-                .map(str -> str.split("\t"))
-                .filter(arr -> arr.length == 2)
-                .map(arr -> new Word(arr[0], arr[1]))
-                .collect(Collectors.toList());
+            return Stream.of(lines)
+                    .map(str -> str.split("\t"))
+                    .filter(arr -> arr.length == 2)
+                    .map(arr -> new Word(arr[0], arr[1]))
+                    .collect(Collectors.toList());
+        }).ifException(e -> Log.e("Java 8 Example", "Utils.readWords", e))
+          .getOrElse(new ArrayList<>());
     }
 }
